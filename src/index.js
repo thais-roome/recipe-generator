77cb9e9e0294c11f0819a5755d14cc8a6b55fd90
thais-roome <tee.szimanski@gmail.com>
2024-01-31 17:11:06 +0000

    Improved user experience with AI generator

--- src/index.js
+++ src/index.js
@@ -10,13 +10,16 @@ function displayRecipe(response) {
 function generateRecipe(event) {
   event.preventDefault();
 
-  let instructionInput = document.querySelector("#user-instructions");
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "dd5d4dt911e62a0b832b58o66f3ab4f3";
   let context =
     "You are a renowed chef worldwide and know how to put together quick and easy recipes that are delicious and healthy. Your mission is to gerenate a recipe in basic html. Make sure to follow user instructions below. Sign the recipe with SheCodes AI";
-  let prompt = `User instructions are: Generate a delicious recipe that is quick and easy to make with ${instructionInput.value}`;
+  let prompt = `User instructions are: Generate a delicious recipe that is quick and easy to make with ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating"> ⏳ Generating the best recipe for you with ${instructionsInput.value}</div>`;
   axios.get(apiURL).then(displayRecipe);
 }
 
