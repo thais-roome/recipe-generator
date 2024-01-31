16ee85847e81bfcc8079d2436b0ef6a2e1964d82
thais-roome <tee.szimanski@gmail.com>
2024-01-31 15:49:02 +0000

    Added AI functions on JavaScript

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generateRecipe(event) {
-  event.preventDefault();
-  // let recipeElement = document.querySelector("#recipe");
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: "Your recipe is here...",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionInput = document.querySelector("#user-instructions");
+  let apiKey = "dd5d4dt911e62a0b832b58o66f3ab4f3";
+  let context =
+    "You are a renowed chef worldwide and know how to put together quick and easy recipes that are delicious and healthy. Your mission is to gerenate a recipe in basic html. Make sure to follow user instructions below. Sign the recipe with SheCodes AI";
+  let prompt = `User instructions are: Generate a delicious recipe that is quick and easy to make with ${instructionInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
